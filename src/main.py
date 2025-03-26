a7e4429b9e089e4ba6c39dc3d9d83b5a55f9b5ca
Salve <salvek.mtb@gmail.com>
2025-03-26 13:08:18 +0100

    Dodanie funkcji informacji oraz funkcji przyrostu informacji

--- src/main.py
+++ src/main.py
@@ -24,51 +24,87 @@ def load_file_to_structure(path):
 
     return data
 
-def count_propability(data):
+def count_probability(data):
 
-    propabilities = {}
+    probabilities = {}
 
     for column, values in data.items():
         num_of_rows = len(values)
         counter = {}
 
-        unique_values = set(values)
-
         for value in values:
             counter[value] = counter.get(value, 0) + 1
 
-        prob = {value: counter.get(value, 0) / num_of_rows for value in unique_values}
-    
-        propabilities[column] = prob
+        prob = {value: count / num_of_rows for value, count in counter.items()}
+        probabilities[column] = prob
 
-    return propabilities
+    return probabilities
 
-def entropy(prop):
-    entropy = 0.0
+def entropy(probabilities):
+    entropy_value = 0.0
 
-    for value, p in prop.items():
+    for p in probabilities.values():
         if p > 0:
-            entropy -= p * math.log2(p)
+            entropy_value -= p * math.log2(p)
 
-    return entropy
-    
+    return entropy_value
+
 def count_entropy(data):
-    
-    propability = count_propability(data)
-    entropy_values = {}
 
-    for column, prob in propability.items():
-        entropy_values[column] = entropy(prob)
+    probabilities = count_probability(data)
+    entropy_values = {column: entropy(prob) for column, prob in probabilities.items()}
     
     return entropy_values
 
+def count_info_x_t(data, decision_column):
+
+    info_values = {}
+    total_rows = len(data[decision_column])
     
-fpath = '../data/testGielda/gielda.txt'      
+    for column in data:
+        if column == decision_column:
+            continue
+        
+        column_values = set(data[column])
+        info_x_t = 0.0
+        
+        for value in column_values:
+            subset_indices = [i for i, v in enumerate(data[column]) if v == value]
+            subset_size = len(subset_indices)
+            
+            decision_values = [data[decision_column][i] for i in subset_indices]
+            decision_counts = {v: decision_values.count(v) for v in set(decision_values)}
+            decision_probs = {k: v / subset_size for k, v in decision_counts.items()}
+            
+            entropy_subset = entropy(decision_probs)
+            info_x_t += (subset_size / total_rows) * entropy_subset
+        
+        info_values[column] = info_x_t
+    
+    return info_values
 
-print(load_file_to_structure(fpath))
-print('\n\n')
-print(count_propability(load_file_to_structure(fpath)))
-print('\n\n')
-print(count_entropy(load_file_to_structure(fpath)))
+def count_gain(data, decision_column):
 
+    total_entropy = entropy(count_probability({decision_column: data[decision_column]})[decision_column])
+    info_x_t_values = count_info_x_t(data, decision_column)
+    
+    gain_values = {column: total_entropy - info for column, info in info_x_t_values.items()}
+    
+    return gain_values
 
+fpath = '../data/testGielda/gielda.txt'
+
+data = load_file_to_structure(fpath)
+
+decision_column = "c4"
+
+print("Dane wczytane ze struktury:")
+print(data)
+print("\nPrawdopodobieństwa:")
+print(count_probability(data))
+print("\nEntropia:")
+print(count_entropy(data))
+print("\nInfo(X, T):")
+print(count_info_x_t(data, decision_column))
+print("\nGain(X, T):")
+print(count_gain(data, decision_column))
